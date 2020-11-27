6246792cb895b6dd0e9626ceb6e648a66390cbce
YT98 <yannistarfa98@gmail.com>
2020-11-26 21:56:26 -0500

    Modified methods and added insert_alias()

--- db.py
+++ db.py
@@ -17,21 +17,30 @@ class Db:
         cursor.execute("CREATE TABLE IF NOT EXISTS population (city_id INT, population INT)")
         cursor.execute("CREATE TABLE IF NOT EXISTS visitors (city_id INT, museum_name VARCHAR(100), visitors INT)")
         cursor.execute("CREATE TABLE IF NOT EXISTS avg_visitors (city_id INT, avg_visitors INT)")
-        cursor.execute("CREATE TABLE IF NOT EXISTS city_aliases (id INT, alias VARCHAR(100))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS aliases (name VARCHAR(100), alias VARCHAR(100))")
+        cursor.close()
 
-    def get_cursor(self):
-        return self.conn.cursor()
-    
     def commit(self):
         self.conn.commit()
 
     def insert_city(self, id, name, country):
-        self.get_cursor().execute("INSERT INTO cities VALUES (%s, '%s', '%s')" % (id, name, country))
+        cursor = self.conn.cursor()
+        cursor.execute("INSERT INTO cities VALUES (%s, '%s', '%s')" % (id, name, country))
+        cursor.close()
+
+    def insert_alias(self, key, alias):
+        cursor = self.conn.cursor()
+        cursor.execute("INSERT INTO aliases VALUES ('%s', '%s')" % (key, alias))
+        cursor.close()
 
     def truncate_tables(self):
-        cursor = self.conn.cursor(buffered=True)
+        cursor = self.conn.cursor()
         cursor.execute("SHOW TABLES")
+        tables = []
         for (table,) in cursor:
-            cursor.execute("DROP TABLE %s" % (table))
+            tables.append(table)
+        for table in tables:
+            cursor.execute("TRUNCATE TABLE %s" % (table))
+        cursor.close()
     
         
