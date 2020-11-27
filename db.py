83583e5d720f4d329348c0424bfcc716991f10f7
YT98 <yannistarfa98@gmail.com>
2020-11-26 21:24:14 -0500

    Created Db class and a few methods

--- db.py
+++ db.py
@@ -1,21 +1,37 @@
-import mysql.connector
+import mysql.connector 
 
-# Returns db cursor and initializes database
-def db_connect():
-    conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="password"
-        )
-    cursor = conn.cursor()
-    ###  Create database and tables
-    cursor.execute("CREATE DATABASE IF NOT EXISTS museum_visitors_db")
-    cursor.execute("USE museum_visitors_db")
-    cursor.execute("CREATE TABLE IF NOT EXISTS cities (id INT, name VARCHAR(100), country VARCHAR(100))")
-    cursor.execute("CREATE TABLE IF NOT EXISTS population (city_id INT, population INT)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS visitors (city_id INT, museum_name VARCHAR(100), visitors INT)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS avg_visitors (city_id INT, avg_visitors INT)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS city_aliases (id INT, alias VARCHAR(100))")
+class Db:
+    def __init__(self):
+        self.init_db() 
 
-    return cursor
+    def init_db(self):
+        self.conn = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="password"
+            )
+        cursor = self.conn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS museum_visitors_db")
+        cursor.execute("USE museum_visitors_db")
+        cursor.execute("CREATE TABLE IF NOT EXISTS cities (id INT, name VARCHAR(100), country VARCHAR(100))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS population (city_id INT, population INT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS visitors (city_id INT, museum_name VARCHAR(100), visitors INT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS avg_visitors (city_id INT, avg_visitors INT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS city_aliases (id INT, alias VARCHAR(100))")
 
+    def get_cursor(self):
+        return self.conn.cursor()
+    
+    def commit(self):
+        self.conn.commit()
+
+    def insert_city(self, id, name, country):
+        self.get_cursor().execute("INSERT INTO cities VALUES (%s, '%s', '%s')" % (id, name, country))
+
+    def truncate_tables(self):
+        cursor = self.conn.cursor(buffered=True)
+        cursor.execute("SHOW TABLES")
+        for (table,) in cursor:
+            cursor.execute("DROP TABLE %s" % (table))
+    
+        
