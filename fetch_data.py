192b4a440f646ff763ab82db358da26cf53a5f43
YT98 <yannistarfa98@gmail.com>
2020-11-26 20:04:11 -0500

    Removed convert to csv and added get_df functions for visitors and population

--- fetch_data.py
+++ fetch_data.py
@@ -4,9 +4,13 @@ import json
 import os
 import xlrd
 import csv
+import pandas as pd
+import numpy as np
+
+LONDON_DS_PATH = os.path.join(os.path.dirname(__file__), "datasets/global-city-population-estimates.xls")
 
 # Fetches and parses data from wikipedia api
-def fetch_museum_data():
+def fetch_visitor_data():
     # Fetch html table from wikipedia api
     url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=list_of_most-visited_museums&section=2&prop=text"
     wiki_data_text = requests.request("GET", url).text
@@ -43,26 +47,32 @@ def fetch_museum_data():
 
     return data
 
-## Source: https://stackoverflow.com/questions/20105118/convert-xlsx-to-csv-correctly-using-python
-def csv_from_excel(in_path, sheet_name, out_path):
-    wb = xlrd.open_workbook(in_path)
-    sh = wb.sheet_by_name(sheet_name)
-    csv_file = open(out_path, 'w')
-    wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-    for rownum in range(sh.nrows):
-        wr.writerow(sh.row_values(rownum))
-    csv_file.close()
+def get_visitor_df():
+    vis_data = fetch_visitor_data()
+    vis_cols = ["Museum Name", "Country", "City", "Visitors per year"]
+    vis_df = pd.DataFrame(vis_data, columns=vis_cols)
+    return vis_df 
 
-# Fetches london population dataset and saves to csv file
+# Fetches london population dataset
 def download_london_dataset():
-    xls_path = os.path.join(os.path.dirname(__file__), "datasets/global-city-population-estimates.xls") 
-    csv_path = os.path.join(os.path.dirname(__file__), "datasets/global-city-population-estimates.csv")
-
-    # Get .xls file
     url = "https://data.london.gov.uk/download/global-city-population-estimates/604a6a6f-2162-4d6b-bcd0-bee051703de1/global-city-population-estimates.xls"
     res = requests.request("GET", url)    
-    open(xls_path, 'wb').write(res.content)
+    open(LONDON_DS_PATH, 'wb').write(res.content)
 
-    # Convert to .csv
-    csv_from_excel(xls_path, 'CITIES-OVER-300K', csv_path)
-    os.remove(xls_path) # Removes original .xls file
+def get_population_df():
+    if not os.path.exists(LONDON_DS_PATH):
+        download_london_dataset()
+
+    pop_df = pd.read_excel(
+        r"datasets/global-city-population-estimates.xls", 
+        "CITIES-OVER-300K",
+        dtype={"2020": np.int64}
+        )
+    pop_df = pop_df[["Country or area", "Urban Agglomeration", "2020"]]
+    pop_df.rename(columns={
+        "Country or area": "Country",
+        "Urban Agglomeration": "City",
+        "2020": "Population",
+    }, inplace=True)
+
+    return pop_df
