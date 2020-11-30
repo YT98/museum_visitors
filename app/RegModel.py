9aff0802c9dae131c8fc8243ae0f8bdbfa042c05
YT98 <yannistarfa98@gmail.com>
2020-11-29 21:32:22 -0500

    Added linear-regression page, linear regression model and server state class

--- app/RegModel.py
+++ app/RegModel.py
@@ -1,20 +1,59 @@
-# from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 class RegModel():
-    def __init__(self, db):
-        # self.model = LinearRegression.model
-        self.db = db
-        self.load_data()
+    def __init__(self, controller):
+        self.model = LinearRegression()
+        self.image_path = "/tmp/plot.jpg"
+        self.load_data(controller)
+        self.train()
 
-    def load_data(self):
-        training_data = self.db.get_training_data()
+    def load_data(self, controller):
+        training_data = controller.get_training_data()
         pop_ls = []
         avg_vis_ls = []
         for row in training_data:
             pop_ls.append(row[0])
             avg_vis_ls.append(row[1])
-        self.pop_ls = pop_ls
-        self.avg_vis_ls = avg_vis_ls
+        pop_ls = np.array(pop_ls).reshape(-1, 1)
+        avg_vis_ls = np.array(avg_vis_ls).reshape(-1, 1)
+        self.x = pop_ls
+        self.y = avg_vis_ls
         
-        
+    def train(self):
+        x = self.x
+        self.model.fit(self.x, self.y)
+
+    def predict(self, population):
+        reg_pop = np.array(population).reshape(-1, 1)
+        pred = self.model.predict(reg_pop)
+        pred = np.round(pred, decimals=0)
+        return pred
+
+    def get_pearson(self):
+        return np.round(np.corrcoef(self.x, self.y)[0][1], decimals=2)
+
+    def get_rsquared(self):
+        return np.round(self.model.score(self.x, self.y), decimals=2)
+
+    def save_plot(self):
+        # Init figure and axes
+        fig = plt.figure()
+        ax = fig.add_axes([0,0,1,1])
+        # Plot data points and Linear Regression line
+        ax.scatter(self.x, self.y, color='b')
+        ax.plot(self.x, self.model.predict(self.x), color='r', linewidth=3)
+        # Format plot
+        ax.set_yticks(np.arange(self.y.min(), self.y.max(), 1000000))
+        ax.set_xticks(np.arange(self.x.min(), self.x.max(), 2000000))
+        ax.set_xlabel("Population")
+        ax.set_ylabel("Average Visitors")
+        # Add r-squared and pearson coefficient text box
+        textstr = "R^2 = {} \n Pearson = {}".format(self.get_rsquared(), self.get_pearson())
+        props = dict(boxstyle='square', facecolor='white', alpha=0.5)
+        ax.text(0.90, 0.95, textstr, transform=ax.transAxes, fontsize=14,
+                horizontalalignment='right', verticalalignment='top', bbox=props)
+        # Save figure
+        plt.savefig(self.image_path, bbox_inches='tight')
--- app/server.py
+++ app/server.py
@@ -1,13 +1,31 @@
 from flask import Flask, redirect, request
 from flask import render_template
 
-# from .RegModel import RegModel
-# reg_model = RegModel()
-
 from .Controller import Controller
+from .RegModel import RegModel
 
-app = Flask(__name__)
-controller = Controller()
+class State:
+    def __init__(self):
+        self.controller = Controller()
+        self.reg_model = None
+        self.has_model = False
+        self.pred = None
+
+    def set_reg_model(self, model):
+        self.reg_model = model
+        self.has_model = True
+
+    def set_pred(self, pred):
+        self.pred = "{:,}".format(pred)
+
+
+state = State()
+app = Flask(
+        __name__,
+        static_url_path='', 
+        static_folder='/tmp'
+    )
+
 
 @app.route("/")
 def home():
@@ -15,21 +33,35 @@ def home():
 
 @app.route("/data")
 def data():
-    data_obj = controller.get_data_page_data()
+    data_obj = state.controller.get_data_page_data()
     return render_template('data.html', pop=data_obj["pop"], vis=data_obj["vis"], avg_vis=data_obj["avg_vis"])
 
 @app.route("/populate")
 def populate():
-    controller.populate()
+    state.controller.populate()
     return ("nothing")
 
 @app.route("/update-population")
 def update_population():
     city_id = request.args.get("city_id")
     population = request.args.get("population")
-    controller.update_population(city_id, population)
+    state.controller.update_population(city_id, population)
     return ("nothing")
 
 @app.route("/linear-regression")
 def linear_regression():
-    return render_template('linear-regression.html')
+    return render_template('linear-regression.html', has_model=state.has_model, pred=state.pred)
+
+@app.route("/linear-regression/create-model")
+def create_model():
+    reg_model = RegModel(state.controller)
+    reg_model.save_plot()
+    state.set_reg_model(reg_model)
+    return ("nothing")
+
+@app.route("/linear-regression/predict")
+def predict():
+    pop = int(request.args.get("population"))
+    pred = state.reg_model.predict(pop)
+    state.set_pred(pred[0][0])
+    return ("nothing")
